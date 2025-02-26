8822e69cb54aa05c3ac37ae87d203dce89c4516f
cqllin <collinjtalik@gmail.com>
2025-02-26 00:37:42 -0500

    edit: fix call to GetPlayers var to not reuse native

--- http.lua
+++ http.lua
@@ -7,7 +7,7 @@ r:Get('/', function(req, res)
     if #players == 0 then
         data.players = "No players online!"
     else
-        for _, player in ipairs(GetPlayers()) do
+        for _, player in ipairs(players) do
             local allIdentifiers = GetAllIdentifiers(player)
             local name = GetPlayerName(player)
             local ping = GetPlayerPing(player)
@@ -40,7 +40,7 @@ r:Get('/:player', function(req, res)
             data.players = "No players online!"
         else
             sentPlayerId = tostring(sentPlayerId)
-            for _, player in ipairs(GetPlayers()) do
+            for _, player in ipairs(players) do
                 if sentPlayerId == player then
                     found = true
                     local allIdentifiers = GetAllIdentifiers(player)
